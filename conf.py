b4ee417ca8a680b588668cd1c3d93a7534eed19a
matianxing <matian388@gmail.com>
2019-05-16 15:53:55 +0800

    update

--- conf.py
+++ conf.py
@@ -1,4 +1,10 @@
 
-CACHE_REDIS_HOST = '172.17.0.6'
-
 SQLALCHEMY_DATABASE_URI = 'postgresql://root:19950403@172.17.0.4/data'
+
+CACHE_CONFIG = {
+    "CACHE_TYPE": "redis",
+    "CACHE_REDIS_HOST": '172.17.0.6',
+    "CACHE_REDIS_PORT": 6379,
+    "CACHE_REDIS_DB": 0,
+    "CACHE_REDIS_PASSWORD": "",
+}
--- util/cache.py
+++ util/cache.py
@@ -1,9 +1,3 @@
 from flask_cache import Cache
-from conf import CACHE_REDIS_HOST
-cache = Cache(config={
-    "CACHE_TYPE": "redis",
-    "CACHE_REDIS_HOST": CACHE_REDIS_HOST,
-    "CACHE_REDIS_PORT": 6379,
-    "CACHE_REDIS_DB": 0,
-    "CACHE_REDIS_PASSWORD": "",
-})
+from conf import CACHE_CONFIG
+cache = Cache(config=CACHE_CONFIG)
