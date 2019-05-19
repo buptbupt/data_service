918d13827741c6a0b4d0c5a30555fa652aed681c
matianxing <matian388@gmail.com>
2019-05-19 18:35:11 +0800

    update

--- util/celery.py
+++ util/celery.py
@@ -3,10 +3,10 @@ from celery import Celery
 from conf import CELERY_CONFIG
 
 celery = Celery()
-celery.conf.update(CELERY_CONFIG)
+celery.conf.update(**CELERY_CONFIG)
 
 
 def create_app():
     app = Flask(__name__)
-    celery.conf.update(CELERY_CONFIG)
+    celery.conf.update(**CELERY_CONFIG)
     return app
