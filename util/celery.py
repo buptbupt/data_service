e1df43fa7667d4e579ebc5fe7cd5a2f1187b9b9c
matianxing <matian388@gmail.com>
2019-06-13 10:38:21 +0800

    update

--- util/celery.py
+++ util/celery.py
@@ -1,12 +1,10 @@
 from flask import Flask
 from celery import Celery
 from conf import CELERY_CONFIG
+from app import app
+
+app.app_context().push()
 
 celery = Celery(__name__, broker=CELERY_CONFIG['CELERY_BROKER_URL'])
 celery.conf.update(**CELERY_CONFIG)
 
-
-def create_app():
-    app = Flask(__name__)
-    celery.conf.update(**CELERY_CONFIG)
-    return app
