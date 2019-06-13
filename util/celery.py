add70bbf4036354613ec4645d9ff6e50f69b9a24
matianxing <matian388@gmail.com>
2019-06-13 11:04:38 +0800

    update

--- util/celery.py
+++ util/celery.py
@@ -4,12 +4,38 @@ from conf import CELERY_CONFIG
 from util.db import db
 from conf import *
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+class FlaskCelery(Celery):
 
-db.init_app(app)
+    def __init__(self, *args, **kwargs):
 
-celery = Celery(__name__, broker=CELERY_CONFIG['CELERY_BROKER_URL'])
+        super(FlaskCelery, self).__init__(*args, **kwargs)
+        self.patch_task()
+
+        if 'app' in kwargs:
+            self.init_app(kwargs['app'])
+
+    def patch_task(self):
+        TaskBase = self.Task
+        _celery = self
+
+        class ContextTask(TaskBase):
+            abstract = True
+
+            def __call__(self, *args, **kwargs):
+                if flask.has_app_context():
+                    return TaskBase.__call__(self, *args, **kwargs)
+                else:
+                    with _celery.app.app_context():
+                        return TaskBase.__call__(self, *args, **kwargs)
+
+        self.Task = ContextTask
+
+    def init_app(self, app):
+        self.app = app
+        self.config_from_object(app.config)
+
+
+celery = FlaskCelery()
+# celery = Celery(__name__, broker=CELERY_CONFIG['CELERY_BROKER_URL'])
 celery.conf.update(**CELERY_CONFIG)
 
