2d68379a683b31db363120390d96d273a67c8ef5
matianxing <matian388@gmail.com>
2019-06-13 10:57:31 +0800

    update

--- util/celery.py
+++ util/celery.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from celery import Celery
 from conf import CELERY_CONFIG
-from app import app
+from util.db import db
 
-app.app_context().push()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 celery = Celery(__name__, broker=CELERY_CONFIG['CELERY_BROKER_URL'])
 celery.conf.update(**CELERY_CONFIG)
